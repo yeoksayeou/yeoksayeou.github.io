432a8b748ff37a63f5dd0b63796edd165cc8d2b9
Yeoksayeou <kaebyeokarchive@yahoo.com>
2025-04-09 14:21:45 +0100

    Update overview.js

--- tda/support/overview.js
+++ tda/support/overview.js
@@ -1,6 +1,6 @@
 // Site overview information
 window.SITE_INFO = {
-    title: "大東亞 - 대동아- Taedonga",
+    title: "大東亞 - 대동아- Taedonga (1942-3)",
     version: "1.0.0",
     sourceLink: "https://db.history.go.kr/modern/level.do?levelId=ma_017",
     sourceName: "국사편찬위원회",
